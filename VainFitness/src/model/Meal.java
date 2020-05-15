a65f68426fb2bc29a9c741633e16c9c33444c8c0
Shazzam Austin <shazzamdaustin@gmail.com>
2020-05-15 15:36:01 -0500

    Added code for FoodItem, Meal and MealPlan (MealPlan not finished though)

--- VainFitness/src/model/Meal.java
+++ VainFitness/src/model/Meal.java
@@ -5,45 +5,45 @@ import java.util.*;
 /**
  * 
  */
-public class Meal {
-
-    /**
-     * Default constructor
-     */
-    public Meal() {
-    }
-
-    /**
-     * 
-     */
+public class Meal
+{
     private String name;
-
-    /**
-     * 
-     */
-    private Set<FoodItem> foodItems;
-
-    /**
-     * 
-     */
+    private ArrayList<FoodItem> foodItems;
     private double totalCaloricValue;
 
 
 
+    /**
+     * Default constructor
+     */
+    public Meal(String name, FoodItem breakfask, FoddItem lunch, FoodItem dinner, double totalCaloricValue)
+    {
+        this.name = name;
+        this.foodItems = new ArrayList<FoodItem>();
+        this.totalCaloricValue = totalCaloricValue;
+    }
 
 
     /**
      * @param foodItem
      */
-    public void addItem(FoodItem foodItem) {
+    public void addItem(FoodItem foodItem)
+    {
         // TODO implement here
+        System.out.println(foodItems); // Display list of food items
+        foodItems.add(foodItem); // Add food item to the arraylist
+        System.out.println(foodItems); // Disply list after food item has been added
     }
 
     /**
      * @param foodItem
      */
-    public void deleteItem(FoodItem foodItem) {
+    public void deleteItem(FoodItem foodItem)
+    {
         // TODO implement here
+        System.out.println(foodItems); // Display the list of food items
+        foodItems.remove(foodItem); // Remove food item from an arraylist
+        System.out.println(foodItems); // Display list after food item has been removed
     }
 
 }
--- VainFitness/src/model/MealPlan.java
+++ VainFitness/src/model/MealPlan.java
@@ -5,52 +5,42 @@ import java.util.*;
 /**
  * 
  */
-public class MealPlan {
-
+public class MealPlan
+{
+    private String name;
+    private Meal breakfast;
+    private Meal lunch;
+    private Meal dinner;
     /**
      * Default constructor
      */
-    public MealPlan() {
+    public MealPlan(String name, Meal breakfast, Meal lunch, Meal dinner)
+    {
+        this.name = name;
+        this.breakfast = breakfast;
+        this.lunch = lunch;
+        this.dinner = dinner;
     }
-
-    /**
-     * 
-     */
-    private String name;
-
-    /**
-     * 
-     */
-    private Meal breakfast;
-
-    /**
-     * 
-     */
-    private Meal lunch;
-
-    /**
-     * 
-     */
-    private Meal dinner;
-
-
-
-
 
 
     /**
      * @param str 
      * @param meal
      */
-    public void addMeal(String str, Meal meal) {
+    public void addMeal(String str, Meal meal)
+    {
         // TODO implement here
+
+
+
     }
 
     /**
      * @param str 
      * @param meal
      */
-    public void deleteMeal(String str, Meal meal) {
+    public void deleteMeal(String str, Meal meal)
+    {
         // TODO implement here
     }
 
@@ -61,7 +51,8 @@ public class MealPlan {
      * @param meal 
      * @param foodItem
      */
-    public void editMeal(String str, String str2, Meal meal, FoodItem foodItem) {
+    public void editMeal(String str, String str2, Meal meal, FoodItem foodItem)
+    {
         // TODO implement here
     }
 
