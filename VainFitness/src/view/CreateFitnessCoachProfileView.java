e6709b471436c7dc8a3e2b427360f8616ceef677
MKellman26 <57738618+MKellman26@users.noreply.github.com>
2020-05-22 19:33:43 -0500

    view final changes

--- VainFitness/src/view/CreateFitnessCoachProfileView.java
+++ VainFitness/src/view/CreateFitnessCoachProfileView.java
@@ -1,16 +1,248 @@
 package view;
 
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JProgressBar;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import GUI.ClientProfileUI;
+import GUI.CreateClientProfileUI;
+import model.fitnessController;
 
 /**
  * 
  */
 public class CreateFitnessCoachProfileView {
 
+	private fitnessController controller;
+	//
+
+	private JFrame frame;
+	private JFrame MainFrame;
+	private String Firstname;
+	private String Lastname;
+	private String Contact;
+	private String Email;
+	private String Gender;
+	private String Username;
+	private String Password;
+	//
+	private JTextField firstname;
+	private JTextField lastname;
+	private JTextField contact;
+	private JTextField email;
+	private JTextField gender;
+
+	private JProgressBar progressBar;
+	private JTextField username;
+	private JPasswordField password1;
+	private JPasswordField password2;
+	
+	JLabel passwordLabel;
+
 	/**
-	 * Default constructor
+	 * Create the application.
 	 */
 	public CreateFitnessCoachProfileView() {
+		initialize();
+	}
+	
+	
+	public void showFrame(JFrame mainFrame) {
+		this.MainFrame = mainFrame;
+		this.controller = fitnessController.getInstance();
+		frame.setVisible(true);
+	}
+	
+	public void hideFrame() {
+		frame.setVisible(false);
+		MainFrame.setVisible(true);
+		frame.dispose();
 	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 670, 450);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton confirmationBtn = new JButton("Confirm");
+		confirmationBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				controller.createTrainerProfile("Trainer",Username,Password,Firstname,Lastname,Contact,Email);
+				System.out.println(controller.getTrainerInfo());
+				if(controller.checkCurrentUser()) {
+					JOptionPane.showMessageDialog(null, "Profile Created!");
+					VainFitnessUIAdmin adminUI = new VainFitnessUIAdmin();
+					adminUI .showFrame(MainFrame);
+					frame.dispose();
+				}else {
+					JOptionPane.showMessageDialog(null, "Profile was not created successfully. Please try again!");
+					hideFrame();
+				}
+			}
+		});
+		confirmationBtn.setBounds(484, 358, 123, 32);
+		frame.getContentPane().add(confirmationBtn);
+		
+		JButton cancelBtn = new JButton("Cancel");
+		cancelBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				hideFrame();
+			}
+		});
+		cancelBtn.setBounds(57, 358, 115, 32);
+		frame.getContentPane().add(cancelBtn);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(29, 68, 589, 277);
+		frame.getContentPane().add(tabbedPane);
+		
+		JPanel panel_4 = new JPanel();
+		tabbedPane.addTab("Member Info", null, panel_4, null);
+		panel_4.setLayout(null);
+		
+		username = new JTextField();
+		username.setBounds(154, 65, 231, 22);
+		panel_4.add(username);
+		username.setColumns(10);
+		
+		password1 = new JPasswordField();
+		password1.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				
+			}
+		});
+		password1.setBounds(154, 113, 231, 22);
+		panel_4.add(password1);
+		
+		password2 = new JPasswordField();
+		password2.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				if(!(String.valueOf(password1.getPassword()).contentEquals(String.valueOf(password2.getPassword())))) {
+					passwordLabel.setText("Password Mismatch!");
+					
+				}else {
+					passwordLabel.setText("Password Match!");
+				}
+				
+			}
+		});
+		password2.setBounds(156, 160, 231, 22);
+		panel_4.add(password2);
+		
+		JLabel lblNewLabel_20 = new JLabel("Username");
+		lblNewLabel_20.setBounds(12, 68, 87, 16);
+		panel_4.add(lblNewLabel_20);
+		
+		JLabel lblNewLabel_21 = new JLabel("Password");
+		lblNewLabel_21.setBounds(12, 116, 87, 16);
+		panel_4.add(lblNewLabel_21);
+		
+		JLabel lblNewLabel_22 = new JLabel("Re-enter Password");
+		lblNewLabel_22.setBounds(12, 163, 110, 16);
+		panel_4.add(lblNewLabel_22);
+		
+		JButton saveMemberInfo = new JButton("save");
+		saveMemberInfo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Username = username.getText();
+				Password = String.valueOf(password2.getPassword());
+				progressBar.setValue(20);
+			}
+		});
+		saveMemberInfo.setBounds(475, 209, 97, 25);
+		panel_4.add(saveMemberInfo);
+		
+		passwordLabel = new JLabel("");
+		passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordLabel.setBounds(208, 195, 125, 16);
+		panel_4.add(passwordLabel);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("PersonalInfo", null, panel, null);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Firstname:");
+		lblNewLabel_1.setBounds(12, 25, 106, 16);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Lastname");
+		lblNewLabel_2.setBounds(12, 79, 56, 16);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Contact");
+		lblNewLabel_3.setBounds(12, 148, 67, 16);
+		panel.add(lblNewLabel_3);
+		
+		firstname = new JTextField();
+		firstname.setBounds(12, 44, 116, 22);
+		panel.add(firstname);
+		firstname.setColumns(10);
+		
+		lastname = new JTextField();
+		lastname.setBounds(12, 101, 116, 22);
+		panel.add(lastname);
+		lastname.setColumns(10);
+		
+		contact = new JTextField();
+		contact.setBounds(12, 169, 116, 22);
+		panel.add(contact);
+		contact.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Email:");
+		lblNewLabel_4.setBounds(231, 25, 56, 16);
+		panel.add(lblNewLabel_4);
+		
+		email = new JTextField();
+		email.setBounds(231, 44, 116, 22);
+		panel.add(email);
+		email.setColumns(10);
+		
+		JButton savePersonalInfo = new JButton("save");
+		savePersonalInfo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(email.getText().equals("")){
+					JOptionPane.showMessageDialog(null, "Please enter values!");
+				}else {
+				Firstname = firstname.getText();
+				Lastname = lastname.getText();
+				Contact = contact.getText();
+				Email = email.getText();
+				progressBar.setValue(40);
+				
+				}
+			}
+		});
+		savePersonalInfo.setBounds(475, 209, 97, 25);
+		panel.add(savePersonalInfo);
+		
+		JLabel lblNewLabel = new JLabel("Create Trainer Profile");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(29, 13, 578, 42);
+		frame.getContentPane().add(lblNewLabel);
+		
+		progressBar = new JProgressBar();
+		progressBar.setBounds(246, 358, 146, 14);
+		frame.getContentPane().add(progressBar);
+	}
 }
--- VainFitness/src/view/EditLMPView.java
+++ VainFitness/src/view/EditLMPView.java
@@ -1,44 +1,78 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
+import GUI.ClientProfileUI;
+import model.Member;
+import model.SomethingSelectedController;
+import model.fitnessController;
+
 /**
  * 
  */
 public class EditLMPView {
+	private fitnessController controller;
+	private SomethingSelectedController controller2;
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					EditLMPView window = new EditLMPView();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JFrame MainFrame;
+	private JButton confirmButton;
+	private JButton cancelButton;
+	private JButton dinnerSaveButton;
+	private JButton lunchSaveButton;
+	private JButton breakfastSaveButton;
+	private JButton newBreakfastMealButton;
+	private JButton newLunchMealButton;
+	private JButton newDinnerMealButton;
+	private JButton deleteButton;
+	private JList<String> breakfastList;
+	private JList<String> lunchList;
+	private JList<String> dinnerList;
+	private DefaultListModel<String> breakfastListModel;
+	private DefaultListModel<String> lunchListModel;
+	private DefaultListModel<String> dinnerListModel;
+	String selectedB;
+	String selectedL;
+	String selectedD;
 
 	/**
 	 * Create the application.
 	 */
-	public EditLMPView() {
+	public EditLMPView(ArrayList<String> meals, String selectedB, String selectedL, String selectedD) {
+		this.selectedB = selectedB;
+		this.selectedD = selectedD;
+		this.selectedL = selectedL;
+		breakfastListModel = new DefaultListModel<String>();
+		lunchListModel = new DefaultListModel<String>();
+		dinnerListModel = new DefaultListModel<String>();
+		addElemListModel(meals);
 		initialize();
 	}
 
+	public void showFrame(JFrame mainFrame) {
+		this.MainFrame = mainFrame;
+		this.controller = fitnessController.getInstance();
+		this.controller2 = SomethingSelectedController.getInstance();
+		frame.setVisible(true);
+	}
+	
+	public void hideFrame() {
+		frame.setVisible(false);
+		MainFrame.dispose();
+		frame.dispose();
+		
+	}
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -56,77 +90,145 @@ public class EditLMPView {
 		tabbedPane.addTab("Breakfast", null, panel, null);
 		panel.setLayout(null);
 
-		JButton btnNewButton_5 = new JButton("Save");
-		btnNewButton_5.setBounds(310, 142, 89, 23);
-		panel.add(btnNewButton_5);
-
-		JButton btnNewButton_6 = new JButton("Create New Meal");
-		btnNewButton_6.setBounds(174, 142, 126, 23);
-		panel.add(btnNewButton_6);
-
-		JList list = new JList();
-		list.setBounds(10, 11, 389, 120);
-		panel.add(list);
+		breakfastSaveButton = new JButton("Save");
+		breakfastSaveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller2.setSelectedB(breakfastList.getSelectedValue());
+			}
+		});
+		breakfastSaveButton.setBounds(310, 142, 89, 23);
+		panel.add(breakfastSaveButton);
+		
+		newBreakfastMealButton = new JButton("Create New Meal");
+		newBreakfastMealButton.setBounds(174, 142, 126, 23);
+		panel.add(newBreakfastMealButton);
+		
+		breakfastList = new JList<String>();
+		breakfastList.setModel(breakfastListModel);
+		breakfastList.setBounds(10, 11, 389, 120);
+		panel.add(breakfastList);
 
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Lunch", null, panel_1, null);
 		panel_1.setLayout(null);
 
-		JButton btnNewButton_4 = new JButton("Save");
-		btnNewButton_4.setBounds(310, 142, 89, 23);
-		panel_1.add(btnNewButton_4);
-
-		JButton btnNewButton_7 = new JButton("Create New Meal");
-		btnNewButton_7.setBounds(174, 142, 126, 23);
-		panel_1.add(btnNewButton_7);
-
-		JList list_1 = new JList();
-		list_1.setBounds(10, 11, 389, 120);
-		panel_1.add(list_1);
+		lunchSaveButton = new JButton("Save");
+		lunchSaveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller2.setSelectedL(lunchList.getSelectedValue());
+			}
+		});
+		lunchSaveButton.setBounds(310, 142, 89, 23);
+		panel_1.add(lunchSaveButton);
+		
+		newLunchMealButton = new JButton("Create New Meal");
+		newLunchMealButton.setBounds(174, 142, 126, 23);
+		panel_1.add(newLunchMealButton);
+		
+		lunchList = new JList<String>();
+		lunchList.setModel(lunchListModel);
+		lunchList.setBounds(10, 11, 389, 120);
+		panel_1.add(lunchList);
 
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Dinner", null, panel_2, null);
 		panel_2.setLayout(null);
 
-		JButton btnNewButton_3 = new JButton("Save");
-		btnNewButton_3.setBounds(310, 142, 89, 23);
-		panel_2.add(btnNewButton_3);
+		dinnerSaveButton = new JButton("Save");
+		dinnerSaveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller2.setSelectedD(dinnerList.getSelectedValue());
+			}
+		});
+		dinnerSaveButton.setBounds(310, 142, 89, 23);
+		panel_2.add(dinnerSaveButton);
+		
+		newDinnerMealButton = new JButton("Create New Meal");
+		newDinnerMealButton.setBounds(174, 142, 126, 23);
+		panel_2.add(newDinnerMealButton);
+		
+		dinnerList = new JList<String>();
+		dinnerList.setModel(dinnerListModel);
+		dinnerList.setBounds(10, 11, 389, 120);
+		panel_2.add(dinnerList);
 
-		JButton btnNewButton_8 = new JButton("Create New Meal");
-		btnNewButton_8.setBounds(174, 142, 126, 23);
-		panel_2.add(btnNewButton_8);
+		confirmButton = new JButton("Confirm");
+		confirmButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!selectedB.equals(controller2.getSelectedB().getName())) {
+					controller2.updateBreakfast();
+				}
+				if(!selectedL.equals(controller2.getSelectedL().getName())) {
+					controller2.updateLunch();
+				}
+				if(!selectedD.equals(controller2.getSelectedD().getName())) {
+					controller2.updateDinner();
+				}
+				Member member = controller.getCurrentUser();
+				System.out.println(member.getUserType());
+				if (member.getUserType().equals("Trainer")) {
+					VainFitnessUITrainer trainerUI = new VainFitnessUITrainer();
+					trainerUI.showFrame(MainFrame);
+					hideFrame();
+				}else if (member.getUserType().equals("Admin")) {
+					VainFitnessUIAdmin adminUI = new VainFitnessUIAdmin();
+					adminUI.showFrame(MainFrame);
+					hideFrame();
+				}else {
+					System.out.println("Error!");
+				}
+			}
+		});
+		confirmButton.setBounds(185, 226, 89, 23);
+		frame.getContentPane().add(confirmButton);
 
-		JList list_2 = new JList();
-		list_2.setBounds(10, 11, 389, 120);
-		panel_2.add(list_2);
+		cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Member member = controller.getCurrentUser();
+				if (member.getUserType().equals("Trainer")) {
+					VainFitnessUITrainer trainerUI = new VainFitnessUITrainer();
+					trainerUI.showFrame(MainFrame);
+					hideFrame();
+				}else if (member.getUserType().equals("Admin")) {
+					VainFitnessUIAdmin adminUI = new VainFitnessUIAdmin();
+					adminUI.showFrame(MainFrame);
+					hideFrame();
+				}else {
+					System.out.println("Error!");
+				}
+			}
+		});
+		cancelButton.setBounds(284, 226, 89, 23);
+		frame.getContentPane().add(cancelButton);
 
-		JPanel panel_3 = new JPanel();
-		tabbedPane.addTab("Snack/Dessert", null, panel_3, null);
-		panel_3.setLayout(null);
-
-		JButton btnNewButton_2 = new JButton("Save");
-		btnNewButton_2.setBounds(310, 142, 89, 23);
-		panel_3.add(btnNewButton_2);
-
-		JButton btnNewButton_9 = new JButton("Create New Meal");
-		btnNewButton_9.setBounds(174, 142, 126, 23);
-		panel_3.add(btnNewButton_9);
-
-		JList list_3 = new JList();
-		list_3.setBounds(10, 11, 389, 120);
-		panel_3.add(list_3);
-
-		JButton btnNewButton = new JButton("Confirm");
-		btnNewButton.setBounds(185, 226, 89, 23);
-		frame.getContentPane().add(btnNewButton);
-
-		JButton btnNewButton_1 = new JButton("Cancel");
-		btnNewButton_1.setBounds(284, 226, 89, 23);
-		frame.getContentPane().add(btnNewButton_1);
-
-		JButton btnNewButton_10 = new JButton("Delete Plan");
-		btnNewButton_10.setBounds(86, 226, 89, 23);
-		frame.getContentPane().add(btnNewButton_10);
+		deleteButton = new JButton("Delete Plan");
+		deleteButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller2.deleteSelectedMP();
+				Member member = controller.getCurrentUser();
+				if (member.getUserType().equals("Trainer")) {
+					VainFitnessUITrainer trainerUI = new VainFitnessUITrainer();
+					trainerUI.showFrame(MainFrame);
+					hideFrame();
+				}else if (member.getUserType().equals("Admin")) {
+					VainFitnessUIAdmin adminUI = new VainFitnessUIAdmin();
+					adminUI.showFrame(MainFrame);
+					hideFrame();
+				}else {
+					System.out.println("Error!");
+				}
+			}
+		});
+		deleteButton.setBounds(86, 226, 89, 23);
+		frame.getContentPane().add(deleteButton);
 	}
 
+	public void addElemListModel(ArrayList<String> meals) {
+		for (String m: meals) {
+			this.breakfastListModel.addElement(m);
+			this.lunchListModel.addElement(m);
+			this.dinnerListModel.addElement(m);
+		}
+	}
 }
